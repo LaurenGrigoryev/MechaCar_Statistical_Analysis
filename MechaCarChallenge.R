346ef02037b78e5e72307e439a6360ff3f468dfe
LaurenSonis <75095346+LaurenSonis@users.noreply.github.com>
2021-03-21 13:18:25 -0400

    RScript for MechaCarChallenge

---
+++ MechaCarChallenge.R
@@ -0,0 +1,7 @@
+library()
+install.packages("dyplr")
+library(dplyr)
+mecha_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+head(mecha_table)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_table) #generate multiple linear regression model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_table)) #generate summary statistics
